5c0de0846268a59b86c60c2307b5c0ca81e248a9
essam-mortada <esammortda2001@gmail.com>
2024-09-11 21:24:25 +0300

    production 1st

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -1,8 +1,17 @@
 @include('layouts.header')
 @if (@session('success'))
-<div class="col-md-3 alert alert-success text-center m-auto mt-3" role="alert">
-{{session('success')}}
-</div>
+<div class="col-sm-12">
+    <div class="alert fade alert-simple alert-success alert-dismissible text-left font__family-montserrat font__size-16 font__weight-light brk-library-rendered rendered show">
+      <button type="button" class="close font__size-18" data-dismiss="alert">
+                                <span aria-hidden="true"><a>
+                <i class="fa fa-times greencross"></i>
+                </a></span>
+                                <span class="sr-only">Close</span>
+                            </button>
+      <i class="start-icon far fa-check-circle faa-tada animated"></i>
+      <strong class="font__weight-semibold">success!</strong> {{session('success')}}
+    </div>
+  </div>
 @endif
 @if (@session('error'))
 <div class="col-md-3 alert alert-danger text-center m-auto mt-3" role="alert">
