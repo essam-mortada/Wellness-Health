9d37ae9d22dfddbc08a838faff7bfb7c86542114
essam-mortada <esammortda2001@gmail.com>
2024-09-19 02:44:26 +0300

    production 3rd

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -51,11 +51,7 @@
                 <div class="form-group">
                     <label for="image">Image:</label>
                     <input type="file" class="form-control" id="image" name="image">
-                    @if($product->image != 'default.png')
-                        <img src="{{ asset('public/products_uploads/'.$product->image) }}" width="50" height="50">
-                    @else
-                        <img src="{{ asset('public/products_uploads/default.png') }}" width="50" height="50">
-                    @endif
+                  
                 </div>
 
                 <button type="submit" class="btn btn-primary">Update Product</button>
