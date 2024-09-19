71d3e7ab45f2098cf5f1d617d2ae4eb0d05b4f21
essam-mortada <esammortda2001@gmail.com>
2024-09-19 03:06:57 +0300

    production 3rd

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -76,7 +76,7 @@
                 </div>
             </div>
             @empty
-            <div class="col-md-6  ftco-animate">
+            <div class="col-md-6 text-center ftco-animate">
                 <h2>No products found!</h2>
             @endforelse
 
