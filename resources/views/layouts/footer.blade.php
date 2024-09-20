a308a78a41479c2e3089644a00cdf981239fa7b2
essam-mortada <esammortda2001@gmail.com>
2024-09-20 17:18:59 +0300

    production 3rd

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -45,7 +45,10 @@
     </div>
   </footer>
 
-
+  <div class="chat-button">
+    <a  href="http://wa.me/201025806537" target="_blank">
+        <i class="icon-chat color-danger" aria-hidden="true"></i>    </a>
+</div>
 
 <!-- loader -->
 <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
