6c6bb2d80e3f42cc0ff1775398b592e80c5b95c5
essam-mortada <esammortda2001@gmail.com>
2024-09-21 22:49:31 +0300

    production 3rd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -216,7 +216,7 @@
     $(document).ready(function(){
         $(".owl-carousel").owlCarousel({
             loop: true, // Infinite loop
-            margin: 5, // Adds space between items
+            margin: 1, // Adds space between items
             autoplay: true, // Enables auto-sliding
             autoplayTimeout: 2000, // Set to 1 second
             autoplayHoverPause: true, // Pause when hovering
