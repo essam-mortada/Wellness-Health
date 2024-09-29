5e56b1e350cdbd51ddad44c9bda0cdedda2e1d0a
essam-mortada <esammortda2001@gmail.com>
2024-09-29 16:15:44 +0300

    production 3rd

--- resources/views/single-blog.blade.php
+++ resources/views/single-blog.blade.php
@@ -7,7 +7,7 @@
       <div class="row no-gutters slider-text align-items-center justify-content-center">
         <div class="col-md-9 ftco-animate text-center">
             <p class="breadcrumbs"><span class="mr-2"><a href="{{route('home')}}">Home</a></span> <span>Blog</span></p>
-          <h1 class="mb-0 bread">Blog</h1>
+          <h1 class="mb-0 bread">{{$blog->title}}</h1>
         </div>
       </div>
     </div>
