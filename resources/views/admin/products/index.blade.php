5ec5aea48ae6ac71584fe6b5eb334a7003dfc0ca
essam-mortada <esammortda2001@gmail.com>
2024-11-17 17:08:49 +0200

    production 6th commit

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-12">
             <div class="d-flex justify-content-between align-items-center mb-3 flex-wrap">
-                <h1>Products</h1>
+                <h3>Products</h3>
                 <a href="{{ route('products.create') }}" class="btn btn-primary mt-2 mt-md-0">Add New Product</a>
             </div>
             <div class="table-responsive">
