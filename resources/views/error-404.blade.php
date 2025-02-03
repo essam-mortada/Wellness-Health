4e1645e4b4157ba9b12f138b06107366076be441
essam-mortada <esammortda2001@gmail.com>
2025-02-03 21:25:17 +0200

    product search edits and product delete handeling

--- resources/views/error-404.blade.php
+++ resources/views/error-404.blade.php
@@ -1,10 +1,10 @@
-<title>error 404</title>
+<title>Not found</title>
 <link rel="stylesheet" href="{{asset('public/assets/css/bootstrap.min.css')}}">
 <link rel="stylesheet" href="{{asset('public/assets/css/style.css')}}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-12 col-md-9 text-center mt-2">
-            <img src="{{ asset('public/assets/images/flat-404-error-template.png') }}" alt="error 404" class="img-404 mt-3">
+            <img src="{{ asset('public/assets/images/flat-404-error-template.png') }}" alt="error 404" class="img-404">
         <a class="btn btn-primary btn-404" href="{{route('home')}}">Go Back Home</a>
         </div>
     </div>
