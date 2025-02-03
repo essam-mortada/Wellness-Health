a5e074440bd79a9d5e32c7b95e73801dd1246ab4
essam-mortada <esammortda2001@gmail.com>
2025-02-03 21:22:47 +0200

    product search edits and product delete handeling

--- resources/views/error-404.blade.php
+++ resources/views/error-404.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-12 col-md-9 text-center mt-2">
             <img src="{{ asset('public/assets/images/flat-404-error-template.png') }}" alt="error 404" class="img-404 mt-3">
-        <a class="btn btn-primary" href="{{route('home')}}">Go Back Home</a>
+        <a class="btn btn-primary btn-404" href="{{route('home')}}">Go Back Home</a>
         </div>
     </div>
 </div>
