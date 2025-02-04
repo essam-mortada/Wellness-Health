ebf8edb66cd28bf3b63b5de61a6aac2cfe2c9304
essam-mortada <esammortda2001@gmail.com>
2025-02-04 22:53:08 +0200

    user auth, wishlist, design edits

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectGuestsTo(fn (Request $request) => route('admin.login'));
+        //$middleware->redirectGuestsTo(fn (Request $request) => route('admin.login'));
         $middleware->web(append: [
             \Illuminate\Session\Middleware\StartSession::class,
         ]);
